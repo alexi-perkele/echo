108808b7d050ba4fb98032d7335c62daa08e781a
alexi <scazochik@gmail.com>
2017-07-27 01:32:35 +0300

    to stream redirection fix

--- IProtocol.hpp
+++ IProtocol.hpp
@@ -37,6 +37,8 @@ class IProtocol {
   virtual void process_data() = 0;
 
   virtual std::ostream& print(std::ostream &os) const = 0;
+  
+  friend std::ostream &operator<<(std::ostream& os, const IProtocol &proto_obj);
 
 };
 
